643c257c4681ced9c5331aa2f95d0c836cd3667b
FrankWang <eternnoir@gmail.com>
2013-08-25 14:24:26 +0000

    add main class for analyziner

---
+++ net/atec/analyzer/Analizer.java
@@ -0,0 +1,16 @@
+package atec.net.atec.analyzer;
+
+public class Analizer {
+	public Analizer(){
+		
+	}
+	
+	public String getDeviceEventFormat(){
+		String ret = null;
+			
+		return ret;
+	}
+	
+	
+	
+}
