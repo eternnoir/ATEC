225bfd71d8e724d30a0a3d09d20596336d12869e
FrankWang <eternnoir@gmail.com>
2013-08-25 14:41:37 +0000

    add copy event type function

--- net/atec/analyzer/ServiceCenter.java
+++ net/atec/analyzer/ServiceCenter.java
@@ -1,7 +1,38 @@
 package atec.net.atec.analyzer;
 
+import java.io.DataOutputStream;
+
 public class ServiceCenter {
-	public static void copyEventType(String Path){
-		
+	public static void copyEventType(String filePath){
+		Process p;
+		try {
+			// Preform su to get root privledges
+			p = Runtime.getRuntime().exec("su", null, null);
+			DataOutputStream os = new DataOutputStream(p.getOutputStream());
+			String mkdircmd = "mkdir /mnt/sdcard/tmp\n";
+			os.writeBytes(mkdircmd);
+			// Attempt to write a file to a root-only
+			String cmd = "getevent -lp >> "+filePath+"/et1\n";
+			os.write(cmd.getBytes());
+			os.flush();cmd = "getevent -p >> "+filePath+"/et2\n";
+			os.write(cmd.getBytes());
+			os.flush();
+			os.writeBytes("exit\n");
+			os.flush();
+			os.close();
+			p.waitFor();
+		} catch (Exception e) {
+			// TODO Code to run in input/output exception
+			// return false;
+			e.printStackTrace();
+		}
 	}
+	/**
+	 * copy event format to tmp file
+	 *	/sdcard/tmp/
+	 */
+	public static void copyEventType(){
+		copyEventType("/sdcard/tmp");
+	}
+	
 }
